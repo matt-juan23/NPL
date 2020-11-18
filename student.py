70c8431c8094e667d15c3867751fcb24c9eb3948
adammbq <adammbq@gmail.com>
2020-11-18 14:51:35 +1100

    student

--- student.py
+++ student.py
@@ -38,7 +38,6 @@ def tokenise(sample):
     """
 
     processed = sample.split()
-
     return processed
 
 def preprocessing(sample):
@@ -46,6 +45,12 @@ def preprocessing(sample):
     Called after tokenising but before numericalising.
     """
 
+    for i in range(len(sample)):
+        #sample[i] = sample[i].replace(".", "")
+        #sample[i] = sample[i].replace(",", "")
+        #sample[i] = sample[i].replace("!", "")
+        #sample[i] = sample[i].replace("-", "")
+        sample[i] = ''.join([letter for letter in sample[i] if letter.isalpha()])
     return sample
 
 def postprocessing(batch, vocab):
@@ -55,8 +60,8 @@ def postprocessing(batch, vocab):
 
     return batch
 
-stopWords = {}
-wordVectors = GloVe(name='6B', dim=300)
+stopWords = {"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"}
+wordVectors = GloVe(name='6B', dim=50)
 
 ################################################################################
 ####### The following determines the processing of label data (ratings) ########
@@ -71,15 +76,35 @@ def convertNetOutput(ratingOutput, categoryOutput):
     outputs a different representation convert the output here.
     """
 
-    #softmax = tnn.Softmax(dim=1)
-    return torch.round(ratingOutput).long, torch.argmax(categoryOutput, 1)
-    #return torch.round(torch.sigmoid(ratingOutput)).long(), torch.argmax(softmax(categoryOutput), 1)
+    softmax = tnn.Softmax(dim=1)
+    sigmoid = tnn.Sigmoid()
+    #return torch.round(ratingOutput).long(), torch.argmax(categoryOutput, 1)
+    return torch.round(sigmoid(ratingOutput)).long(), torch.argmax(softmax(categoryOutput), 1)
     #return torch.argmax(ratingOutput, 1), torch.argmax(categoryOutput, 1)
     #return np.argmax(ratingOutput), np.argmax(categoryOutput)
 
 ################################################################################
 ###################### The following determines the model ######################
 ################################################################################
+
+class loss(tnn.Module):
+    """
+    Class for creating the loss function.  The labels and outputs from your
+    network will be passed to the forward method during training.
+    """
+
+    def __init__(self):
+        super(loss, self).__init__()
+
+    def forward(self, ratingOutput, categoryOutput, ratingTarget, categoryTarget):
+        #ratingOutput, categoryOutput, ratingTarget, categoryTarget = ratingOutput.float(), categoryOutput.float(), ratingTarget.float(), categoryTarget.float()
+        ratingLoss = tnn.BCEWithLogitsLoss()
+        catLoss = tnn.CrossEntropyLoss()
+        loss1 = ratingLoss(ratingOutput.squeeze(1), ratingTarget.float())
+        loss2 = catLoss(categoryOutput, categoryTarget)
+        #print(loss1, loss2)
+        return loss1 + loss2
+
 '''
 # REVISION 1
 class network(tnn.Module):
@@ -112,43 +137,25 @@ class network(tnn.Module):
         return self.l1(hid1.squeeze(0)), softmax(self.l2(hid2.squeeze(0)))
 '''
 
-class loss(tnn.Module):
-    """
-    Class for creating the loss function.  The labels and outputs from your
-    network will be passed to the forward method during training.
-    """
-
-    def __init__(self):
-        super(loss, self).__init__()
-
-    def forward(self, ratingOutput, categoryOutput, ratingTarget, categoryTarget):
-        #ratingOutput, categoryOutput, ratingTarget, categoryTarget = ratingOutput.float(), categoryOutput.float(), ratingTarget.float(), categoryTarget.float()
-        ratingLoss = tnn.BCEWithLogitsLoss()
-        catLoss = tnn.CrossEntropyLoss()
-        #print(ratingOutput.dtype, ratingTarget.dtype, categoryOutput.dtype, categoryTarget.dtype)
-        #print(ratingOutput.shape, categoryOutput.shape)
-        loss1 = ratingLoss(ratingOutput.squeeze(1), ratingTarget.float())
-        loss2 = catLoss(categoryOutput, categoryTarget)
-        return loss1 + loss2
-
-
+'''
+#pretty cool
 class network(tnn.Module):
     def __init__(self):
         super(network, self).__init__()
-        self.rnn1 = tnn.LSTM(300,
-                            200,
+        self.rnn1 = tnn.LSTM(50,
+                            128,
                             num_layers=2,
                             bidirectional=True,
                             dropout=0.5)
-        self.fc1 = tnn.Linear(200*2, 1)
+        self.fc1 = tnn.Linear(128*2, 1)
         self.dropout1 = tnn.Dropout(0.5)
 
-        self.rnn2 = tnn.LSTM(300,
-                            200,
+        self.rnn2 = tnn.LSTM(50,
+                            128,
                             num_layers=2,
                             bidirectional=True,
                             dropout=0.5)
-        self.fc2 = tnn.Linear(200*2, 5)
+        self.fc2 = tnn.Linear(128*2, 5)
         self.dropout2 = tnn.Dropout(0.5)
 
     def forward(self, input, length):
@@ -160,17 +167,83 @@ class network(tnn.Module):
         dropout1 = self.dropout1(input)
         dropout2 = self.dropout2(input)
 
-        output1, (hidden1, cell1) = self.rnn1(dropout1)
-        output2, (hidden2, cell2) = self.rnn2(dropout2)
+        output1, (hidden1, _) = self.rnn1(dropout1)
+        output2, (hidden2, _) = self.rnn2(dropout2)
         #print(hidden1.shape)
         hidden1 = self.dropout1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1))
         hidden2 = self.dropout2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
         #print(hidden1.shape, hidden2.shape)
 
+        #return sigmoid(self.fc1(hidden1)), softmax(self.fc2(hidden2))
+        return self.fc1(hidden1), self.fc2(hidden2)
+        #return self.fc1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1)), self.fc2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
+'''
+#pretty cool
+class network(tnn.Module):
+    def __init__(self):
+        super(network, self).__init__()
+        self.rnn1 = tnn.LSTM(50,
+                            128,
+                            num_layers=2,
+                            bidirectional=True,
+                            dropout=0.5)
+        self.fc1 = tnn.Linear(128*2, 1)
+        self.dropout1 = tnn.Dropout(0.5)
+
+        self.rnn2 = tnn.LSTM(50,
+                            128,
+                            num_layers=2,
+                            bidirectional=True,
+                            dropout=0.5)
+        self.fc2 = tnn.Linear(128*2, 5)
+        self.dropout2 = tnn.Dropout(0.5)
+
+    def forward(self, input, length):
+        input = input.float() # shape [batch size, sentence length, embedding size]
+        #print(input[0].shape)
+        # reshape it to form [sentence length, batch size, embedding size]
+        #input = input.reshape([-1, length.shape[0], 50])
+        input = torch.transpose(input, 0, 1)
+        dropout1 = self.dropout1(input)
+        dropout2 = self.dropout2(input)
+
+        output1, (hidden1, _) = self.rnn1(dropout1)
+        output2, (hidden2, _) = self.rnn2(dropout2)
+        #print(hidden1.shape)
+        hidden1 = self.dropout1(torch.cat((hidden1[-1,:,:], hidden1[-2,:,:]), dim=1))
+        hidden2 = self.dropout2(torch.cat((hidden2[-1,:,:], hidden2[-2,:,:]), dim=1))
+        #print(hidden1.shape, hidden2.shape)
+
+        #return sigmoid(self.fc1(hidden1)), softmax(self.fc2(hidden2))
+        return self.fc1(hidden1), self.fc2(hidden2)
+        #return self.fc1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1)), self.fc2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
+
+'''
+# pretty shit ngl
+class network(tnn.Module):
+    def __init__(self):
+        super(network, self).__init__()
+        self.ratingRNN = tnn.RNN(input_size=50, hidden_size=200, num_layers=2, bidirectional=True)
+        self.ratingFc = tnn.Linear(800, 1)
+
+        self.catRNN = tnn.RNN(input_size=50, hidden_size=200, num_layers=2, bidirectional=True)
+        self.catFc = tnn.Linear(800, 5)
+
+    def forward(self, input, length):
+        input = input.float() # shape [batch size, sentence length, embedding size]
+        # reshape it to form [sentence length, batch size, embedding size]
+        input = torch.transpose(input, 0, 1)
+        ratingOutput, ratingHidden = self.ratingRNN(input)
+        catOutput, catHidden = self.catRNN(input)
+        #print(ratingHidden.shape, catHidden.shape, length.shape)
         sigmoid = tnn.Sigmoid()
         softmax = tnn.Softmax(dim=1)
-        return self.fc1(hidden1), self.fc2(hidden2)
-
+        ratingHidden = torch.cat((ratingHidden[-4,:,:], ratingHidden[-3,:,:], ratingHidden[-2,:,:], ratingHidden[-1,:,:]), dim=1)
+        catHidden = torch.cat((catHidden[-4,:,:], catHidden[-3,:,:], catHidden[-2,:,:], catHidden[-1,:,:]), dim=1)
+        #print(ratingHidden.shape, catHidden.shape)
+        return self.ratingFc(ratingHidden.squeeze(0)), self.catFc(catHidden.squeeze(0))
+        #return sigmoid(self.ratingFc(ratingHidden)), softmax(self.catFc(catHidden))
+'''
 net = network()
 lossFunc = loss()
 
@@ -179,11 +252,13 @@ lossFunc = loss()
 ################################################################################
 
 trainValSplit = 0.8
-batchSize = 32
-epochs = 10
+batchSize = 64
+epochs = 20
 #optimiser = toptim.SGD(net.parameters(), lr=0.01)
+#optimiser = toptim.Adam(net.parameters(), lr=0.0005)
 optimiser = toptim.Adam(net.parameters(), lr=0.005)
 
 # 79.26 lr=0.005 dropout=0.5 arch=lstm->linear
-
+# 83.91 lr=0.0005 dropout=0.5 same arch + preprocessing 20 epochs
+# 40 epochs went about 0.5 better score
 
