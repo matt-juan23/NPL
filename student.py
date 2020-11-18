d90f416201f9f2934f46f7202b4b8ab09b67786e
adammbq <adammbq@gmail.com>
2020-11-18 23:25:48 +1100

    change fully size

--- student.py
+++ student.py
@@ -272,7 +272,7 @@ batchSize = 64
 epochs = 20
 #optimiser = toptim.SGD(net.parameters(), lr=0.01)
 #optimiser = toptim.Adam(net.parameters(), lr=0.0005)
-optimiser = toptim.Adam(net.parameters(), lr=0.005)
+optimiser = toptim.Adam(net.parameters(), lr=0.0001)
 
 # 79.26 lr=0.005 dropout=0.5 arch=lstm->linear
 # 83.91 lr=0.0005 dropout=0.5 same arch + preprocessing 20 epochs
