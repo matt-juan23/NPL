fb7edf4b3a7dfe51aa95d6817ac9a57374e17c08
adammbq <adammbq@gmail.com>
2020-11-18 18:49:44 +1100

    change fully size

--- student.py
+++ student.py
@@ -188,19 +188,19 @@ class network(tnn.Module):
     def __init__(self):
         super(network, self).__init__()
         self.rnn1 = tnn.LSTM(300,
-                            128,
+                            256,
                             num_layers=3,
                             bidirectional=True,
                             dropout=0.6)
-        self.fc1 = tnn.Linear(128*2, 1)
+        self.fc1 = tnn.Linear(256*2, 1)
         self.dropout1 = tnn.Dropout(0.6)
 
         self.rnn2 = tnn.LSTM(300,
-                            128,
+                            256,
                             num_layers=3,
                             bidirectional=True,
                             dropout=0.6)
-        self.fc2 = tnn.Linear(128*2, 5)
+        self.fc2 = tnn.Linear(256*2, 5)
         self.dropout2 = tnn.Dropout(0.6)
 
     def forward(self, input, length):
