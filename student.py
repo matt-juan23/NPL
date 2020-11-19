fc59643648bd0eac6a0e57c4c362728de0398336
adammbq <adammbq@gmail.com>
2020-11-19 14:25:17 +1100

    final

--- student.py
+++ student.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 """
 student.py
-
 UNSW COMP9444 Neural Networks and Deep Learning
-
 You may modify this file however you wish, including creating additional
 variables, functions, classes, etc., so long as your code runs with the
 hw2main.py file unmodified, and you are only using the approved packages.
-
 You have been given some default values for the variables stopWords,
 wordVectors, trainValSplit, batchSize, epochs, and optimiser, as well as a basic
 tokenise function.  You are encouraged to modify these to improve the
 performance of your model.
-
 The variable device may be used to refer to the CPU/GPU being used by PyTorch.
 You may change this variable in the config.py file.
-
 You may only use GloVe 6B word vectors as found in the torchtext package.
 """
 
@@ -47,11 +42,8 @@ def preprocessing(sample):
     """
 
     for i in range(len(sample)):
-        #sample[i] = sample[i].replace(".", "")
-        #sample[i] = sample[i].replace(",", "")
-        #sample[i] = sample[i].replace("!", "")
-        #sample[i] = sample[i].replace("-", "")
-        sample[i] = ''.join([letter for letter in sample[i] if letter.isalpha()])
+
+        sample[i] = ''.join([letter for letter in sample[i] if letter.isalpha() or letter.isnumeric()])
     return sample
 
 def postprocessing(batch, vocab):
@@ -79,10 +71,7 @@ def convertNetOutput(ratingOutput, categoryOutput):
 
     softmax = tnn.Softmax(dim=1)
     sigmoid = tnn.Sigmoid()
-    #return torch.round(ratingOutput).long(), torch.argmax(categoryOutput, 1)
     return torch.round(sigmoid(ratingOutput)).long(), torch.argmax(softmax(categoryOutput), 1)
-    #return torch.argmax(ratingOutput, 1), torch.argmax(categoryOutput, 1)
-    #return np.argmax(ratingOutput), np.argmax(categoryOutput)
 
 ################################################################################
 ###################### The following determines the model ######################
@@ -98,208 +87,67 @@ class loss(tnn.Module):
         super(loss, self).__init__()
 
     def forward(self, ratingOutput, categoryOutput, ratingTarget, categoryTarget):
-        #ratingOutput, categoryOutput, ratingTarget, categoryTarget = ratingOutput.float(), categoryOutput.float(), ratingTarget.float(), categoryTarget.float()
         ratingLoss = tnn.BCEWithLogitsLoss()
         catLoss = tnn.CrossEntropyLoss()
         loss1 = ratingLoss(ratingOutput.squeeze(1), ratingTarget.float())
         loss2 = catLoss(categoryOutput, categoryTarget)
-        print("loss")
-        print(loss1, loss2)
-        # return (loss1 + loss2) 
-        # return (loss1 + loss2)/2
-        return torch.log(loss1)/128 + torch.log(loss2)/128
-
-'''
-# REVISION 1
-class network(tnn.Module):
-    """
-    Class for creating the neural network.  The input to your network will be a
-    batch of reviews (in word vector form).  As reviews will have different
-    numbers of words in them, padding has been added to the end of the reviews
-    so we can form a batch of reviews of equal length.  Your forward method
-    should return an output for both the rating and the business category.
-    """
-
-    def __init__(self):
-        super(network, self).__init__()
-        self.r1 = tnn.RNN(50, 200)
-        self.r2 = tnn.RNN(50, 200)
-
-        self.l1 = tnn.Linear(200, 1)
-        self.l2 = tnn.Linear(200, 5)
-
-    def forward(self, input, length):
-        input = input.float()
-        #print(input.shape, length.shape)
-        # reshape it to form [sentence length, batch size, embedding size]
-        out1, hid1 = self.r1(input.reshape([-1, length.shape[0], 50]))
-        out2, hid2 = self.r2(input.reshape([-1, length.shape[0], 50]))
-        #print(hid1.shape, hid2.shape)
-        softmax = tnn.Softmax(dim=1)
-        #sigmoid = tnn.Sigmoid()
-        #print((self.l1(hid1.squeeze(0))))
-        return self.l1(hid1.squeeze(0)), softmax(self.l2(hid2.squeeze(0)))
-'''
-
-'''
-#pretty cool
-class network(tnn.Module):
-    def __init__(self):
-        super(network, self).__init__()
-        self.rnn1 = tnn.LSTM(50,
-                            128,
-                            num_layers=2,
-                            bidirectional=True,
-                            dropout=0.5)
-        self.fc1 = tnn.Linear(128*2, 1)
-        self.dropout1 = tnn.Dropout(0.5)
-
-        self.rnn2 = tnn.LSTM(50,
-                            128,
-                            num_layers=2,
-                            bidirectional=True,
-                            dropout=0.5)
-        self.fc2 = tnn.Linear(128*2, 5)
-        self.dropout2 = tnn.Dropout(0.5)
-
-    def forward(self, input, length):
-        input = input.float() # shape [batch size, sentence length, embedding size]
-        #print(input[0].shape)
-        # reshape it to form [sentence length, batch size, embedding size]
-        #input = input.reshape([-1, length.shape[0], 50])
-        input = torch.transpose(input, 0, 1)
-        dropout1 = self.dropout1(input)
-        dropout2 = self.dropout2(input)
-
-        output1, (hidden1, _) = self.rnn1(dropout1)
-        output2, (hidden2, _) = self.rnn2(dropout2)
-        #print(hidden1.shape)
-        hidden1 = self.dropout1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1))
-        hidden2 = self.dropout2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
-        #print(hidden1.shape, hidden2.shape)
-
-        #return sigmoid(self.fc1(hidden1)), softmax(self.fc2(hidden2))
-        return self.fc1(hidden1), self.fc2(hidden2)
-        #return self.fc1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1)), self.fc2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
-'''
-#pretty cool
-
+        return loss1 + loss2
 ''' Justification
 LSTM
-
 Loss function
-
 '''
 
 class network(tnn.Module):
     def __init__(self):
         super(network, self).__init__()
         self.rnn1 = tnn.LSTM(300,
-                            512,
+                            256,
                             num_layers=2,
                             bidirectional=True,
+                            batch_first=True,
                             dropout=0.6)
 
         self.rnn1fc1 = tnn.Linear(256*2, 128)
         self.rnn1fc2 = tnn.Linear(128, 1)
+
         self.dropout1 = tnn.Dropout(0.6)
 
 
+
+
         self.rnn2 = tnn.LSTM(300,
-                            512,
-                            num_layers=3,
+                            256,
+                            num_layers=2,
                             bidirectional=True,
+                            batch_first=True,
                             dropout=0.6)
 
-        self.rnn2fc1 = tnn.Linear(512*2, 512)
-        self.rnn2fc2 = tnn.Linear(512, 5)
+        self.rnn2fc1 = tnn.Linear(256*2, 256*2)
+        self.rnn2fc2 = tnn.Linear(256*2, 5)
+
         self.dropout2 = tnn.Dropout(0.6)
 
     def forward(self, input, length):
         input = input.float() # shape [batch size, sentence length, embedding size]
-        #print(input[0].shape)
-        # reshape it to form [sentence length, batch size, embedding size]
-        #input = input.reshape([-1, length.shape[0], 50])
-        input = torch.transpose(input, 0, 1)
+
         dropout1 = self.dropout1(input)
         dropout2 = self.dropout2(input)
 
-        
-        #print(hidden1.shape)
+        relu = tnn.ReLU()
+
         # network 1
         output1, (hidden1, _) = self.rnn1(dropout1)
         hidden1 = self.dropout1(torch.cat((hidden1[-1,:,:], hidden1[-2,:,:]), dim=1))
         hidden1 = self.rnn1fc1(hidden1)
-        hidden1 = torch.relu(hidden1)
-        
 
         # network 2
         output2, (hidden2, _) = self.rnn2(dropout2)
         hidden2 = self.dropout2(torch.cat((hidden2[-1,:,:], hidden2[-2,:,:]), dim=1))
         hidden2 = self.rnn2fc1(hidden2)
-        hidden2 = torch.relu(hidden2)
 
-        #print(hidden1.shape, hidden2.shape)
-
-        #return tnn.sigmoid(self.fc1(hidden1)), tnn.softmax(self.fc2(hidden2))
-        #return self.fc1(torch.sigmoid(hidden1)), self.fc2(torch.sigmoid(hidden2))
-        return self.rnn1fc2(hidden1), self.rnn2fc1(hidden2)
-        #return self.fc1(torch.cat((hidden1[-2,:,:], hidden1[-1,:,:]), dim=1)), self.fc2(torch.cat((hidden2[-2,:,:], hidden2[-1,:,:]), dim=1))
+        return self.rnn1fc2(self.dropout1(relu(hidden1))), self.rnn2fc2(self.dropout2(relu(hidden2)))
 
 
-'''
-class convNet(cnn.Module):
-    
-    def __init__(self):
-        self.super(convNet, self).__init__()
-
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
-        
-        self.conv_0 = nn.Conv2d(in_channels = 1, 
-                                out_channels = n_filters, 
-                                kernel_size = (filter_sizes[0], embedding_dim))
-        
-        self.conv_1 = nn.Conv2d(in_channels = 1, 
-                                out_channels = n_filters, 
-                                kernel_size = (filter_sizes[1], embedding_dim))
-        
-        self.conv_2 = nn.Conv2d(in_channels = 1, 
-                                out_channels = n_filters, 
-                                kernel_size = (filter_sizes[2], embedding_dim))
-        
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
-        
-        self.dropout = nn.Dropout(dropout)
-    def forward(self):
-        pass
-
-'''
-'''
-# pretty shit ngl
-class network(tnn.Module):
-    def __init__(self):
-        super(network, self).__init__()
-        self.ratingRNN = tnn.RNN(input_size=50, hidden_size=200, num_layers=2, bidirectional=True)
-        self.ratingFc = tnn.Linear(800, 1)
-
-        self.catRNN = tnn.RNN(input_size=50, hidden_size=200, num_layers=2, bidirectional=True)
-        self.catFc = tnn.Linear(800, 5)
-
-    def forward(self, input, length):
-        input = input.float() # shape [batch size, sentence length, embedding size]
-        # reshape it to form [sentence length, batch size, embedding size]
-        input = torch.transpose(input, 0, 1)
-        ratingOutput, ratingHidden = self.ratingRNN(input)
-        catOutput, catHidden = self.catRNN(input)
-        #print(ratingHidden.shape, catHidden.shape, length.shape)
-        sigmoid = tnn.Sigmoid()
-        softmax = tnn.Softmax(dim=1)
-        ratingHidden = torch.cat((ratingHidden[-4,:,:], ratingHidden[-3,:,:], ratingHidden[-2,:,:], ratingHidden[-1,:,:]), dim=1)
-        catHidden = torch.cat((catHidden[-4,:,:], catHidden[-3,:,:], catHidden[-2,:,:], catHidden[-1,:,:]), dim=1)
-        #print(ratingHidden.shape, catHidden.shape)
-        return self.ratingFc(ratingHidden.squeeze(0)), self.catFc(catHidden.squeeze(0))
-        #return sigmoid(self.ratingFc(ratingHidden)), softmax(self.catFc(catHidden))
-'''
 net = network()
 lossFunc = loss()
 
@@ -307,21 +155,7 @@ lossFunc = loss()
 ################## The following determines training options ###################
 ################################################################################
 
-trainValSplit = 0.9
+trainValSplit = 0.8
 batchSize = 128
-epochs = 1
-#optimiser = toptim.SGD(net.parameters(), lr=0.01)
-#optimiser = toptim.Adam(net.parameters(), lr=0.0005)
-optimiser = toptim.Adam(net.parameters(), lr=0.0002)
-
-# 79.26 lr=0.005 dropout=0.5 arch=lstm->linear
-# 83.91 lr=0.0005 dropout=0.5 same arch + preprocessing 20 epochs
-# 40 epochs went about 0.5 better score
-
-# TrainSplit: 0.9,300d, 10 epoches, adam 0.001 lr --> 86.9
-
-# layers
-# Adam lr
-# loss func
-# trainValSplit
-# epochs
+epochs = 20
+optimiser = toptim.Adam(net.parameters(), lr=0.0005)
